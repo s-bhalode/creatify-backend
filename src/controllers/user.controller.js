6092c13ce86ac96189dd61f4646a0b05af28cbcf
Hardik panwar <hardikpanwar63@gmail.com>
2022-07-18 13:54:43 +0530

    fetch and updated designer data (designer-profile-settings)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,17 +16,46 @@ exports.adminDasboard = (req, res) => {
 exports.recruiterDasboard = (req, res) => {
     res.status(200).send("Recruiter content");
 }
+//   finding designer data by id (designer-profile-settings) 
 exports.find = (req, res) => {
-    const id = req.query.id;
+    // console.log(req.params);
+    const id = req.params.id;
+    // console.log(id);
     User.findById(id)
         .then(data => {
             if(!data){
                 res.status(404).send({ message : "Not found user with id " + id });
             }else{
                 res.send(data);
-            }
+                // console.log(data);
+            }  
         })
         .catch(err => {
             res.status(500).send({ message : "Error retrieving user with id " + id });
         })
+}
+
+
+//   Updating designer-profile-settings data 
+exports.update = (req, res) => {
+    if (!req.body) {
+        return res
+            .status(400)
+            .send({ message: "Data to update can not be empty" })
+    }
+    const id = req.params.id;
+    // console.log(req.params);
+    User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot Update  user with ${id}.Maybe user not found` })
+            } else {
+                res.send(data);
+                // console.log(data);
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: " Error Update user information" })
+        })
+
 }
